49a00691d08e578c9af984410e5a9baf8ec2f10d
青木峻志 <liondrop409@yahoo.co.jp>
2020-02-08 15:21:23 +0900

    add d_make_library

---
+++ d_make_library.py
@@ -0,0 +1,7 @@
+def make_library():
+    cards_ = []
+    for i in range(2,15):
+        for j in [1,2,3,4]:
+            cards_.append(int(str(i) + str(j)))
+    random.shuffle(cards_)
+    return cards_
