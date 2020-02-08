e46b27c12751684e9e464994972560fadee73487
青木峻志 <liondrop409@yahoo.co.jp>
2020-02-08 16:39:34 +0900

    change sample1

--- sample1.py
+++ sample1.py
@@ -1,19 +1,21 @@
-import numpy as np
-import random
-import collections
 
 from defines.d_get_keys_from_value import *
 from defines.d_make_library import *
 from defines.d_judge_game import *
 from defines.d_judge_hand import *
 
-hand = make_library()[:7]
-hand2 = make_library()[:7]
-kekka = judge_hand(hand)
-kekka2 = judge_hand(hand2)
-print(hand)
-print(hand2)
-print(kekka)
-print(kekka2)
-print(judge_game([kekka, kekka2]))
+library_card = make_library()
+
+hand = [library_card.pop(0) for _ in range(2)]
+hand2 = [library_card.pop(0) for _ in range(2)]
+field_card = [library_card.pop(0) for _ in range(5)]
+kekka = judge_hand(hand+field_card)
+kekka2 = judge_hand(hand2+field_card)
+
+print("player0's hand:",hand)
+print("player1's hand:",hand2)
+print("field card:",field_card)
+print("player0's role:",kekka)
+print("player1's role:",kekka2)
+print("win player:",judge_game([kekka, kekka2]))
 
